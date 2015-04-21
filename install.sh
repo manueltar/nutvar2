ec88ac8ffb0832405a48a64a6f22c1a313a7c8e7
manueltar <manueltar@hotmail.com>
2015-04-21 19:13:05 -0400

    modified install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,18 @@
 
 echo Decompressing CDS_genomic_coordinates_full_compresed.txt.tar.gz
 tar -xzvf data/build_tables/CDS_genomic_coordinates_full_compresed.txt.tar.gz -C data/build_tables/
-rm Decompressing CDS_genomic_coordinates_full_compresed.txt.tar.gz
+rm data/build_tables/CDS_genomic_coordinates_full_compresed.txt.tar.gz
 
 echo Decompressing snpEff v3_6
 mkdir SOFTWARE/snpEff
-unzip SOFTWARE/snpEff_v3_6_core.zip -C SOFTWARE/snpEff/
+unzip SOFTWARE/snpEff_v3_6_core.zip -d ./SOFTWARE/snpEff/
 rm SOFTWARE/snpEff_v3_6_core.zip
 
 # The genome files are obtained from ELSEWHERE (#ISSUE 1)
 
 echo obtaining the human genome version GRCh37.75
 scp -r /home/manueltar/Desktop/Proyecto_NutVar2/snpEff_v3_6_GRCh37.75.zip SOFTWARE/snpEff
-unzip SOFTWARE/snpEff/snpEff_v3_6_GRCh37.75.zip -C SOFTWARE/snpEff/
+unzip SOFTWARE/snpEff/snpEff_v3_6_GRCh37.75.zip -d ./SOFTWARE/snpEff/
 rm SOFTWARE/snpEff/snpEff_v3_6_GRCh37.75.zip
 
 
