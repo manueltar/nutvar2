ced43301ee77264678d5babdddfbb2bdbbadb532
manueltar <manueltar@hotmail.com>
2015-04-22 18:21:37 -0400

    created_exec_nutvar2

--- nutvar2.sh
+++ nutvar2.sh
@@ -7,3 +7,5 @@ mkdir data/intermediate/
 
 java -Xmx4g -jar SOFTWARE/snpEff/snpEff.jar eff -c SOFTWARE/snpEff/snpEff.config -v GRCh37.75 -lof -csvStats -nextProt -sequenceOntology test/example.vcf > data/intermediate/example_eff.vcf
 
+mv snpEff_genes.txt data/intermediate/
+mv snpEff_summary.csv data/intermediate/
