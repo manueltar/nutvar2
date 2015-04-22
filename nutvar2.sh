6272e9ad3a3800e03b081d04794d4860ef458849
manueltar <manueltar@hotmail.com>
2015-04-22 18:12:22 -0400

    created_exec_nutvar2

---
+++ nutvar2.sh
@@ -0,0 +1,9 @@
+
+# This line creates the subdirectory data/intermediate
+
+mkdir data/intermediate/
+
+# This line runs snpEff with example file
+
+java -Xmx4g -jar SOFTWARE/snpEff/snpEff.jar eff -c SOFTWARE/snpEff/snpEff.config -v GRCh37.75 -lof -csvStats -nextProt -sequenceOntology test/example.vcf > data/intermediate/example_eff.vcf
+
